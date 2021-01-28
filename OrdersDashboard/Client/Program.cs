fe1aca732c6cc0aa53082f724010c889264fc81a
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-01-28 18:12:40 +0100

    no comments

--- OrdersDashboard/Client/Program.cs
+++ OrdersDashboard/Client/Program.cs
@@ -9,7 +9,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OrdersDashboard.Client.NorthwindServices;
 using Syncfusion.Blazor;
+using OrdersDashboard.Client.Spinner;
+using Microsoft.AspNetCore.Components;
 
+// https://bipinpaul.com/posts/display-spinner-on-each-api-call-automatically-in-blazor
 
 namespace OrdersDashboard.Client
 {
@@ -26,6 +29,23 @@ namespace OrdersDashboard.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<SpinnerService>();
+
+            builder.Services.AddScoped<SpinnerHttpMessageHandler>();
+
+
+            builder.Services.AddScoped(s =>
+            {
+                var accessTokenHandler = s.GetRequiredService<SpinnerHttpMessageHandler>();
+
+                accessTokenHandler.InnerHandler = new HttpClientHandler();
+                var uriHelper = s.GetRequiredService<NavigationManager>();
+                return new HttpClient(accessTokenHandler)
+                {
+                    BaseAddress = new Uri(uriHelper.BaseUri)
+                };
+            });
 
             builder.Services.AddScoped<ProductsService>();
             builder.Services.AddScoped<StatisticsService>();
---
+++ OrdersDashboard/Client/Spinner/SpinnerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// https://bipinpaul.com/posts/display-spinner-on-each-api-call-automatically-in-blazor
+
+namespace OrdersDashboard.Client.Spinner
+{
+	public class SpinnerService
+	{
+		public event Action OnShow;
+		public event Action OnHide;
+
+		public void Show()
+		{
+			OnShow?.Invoke();
+		}
+
+		public void Hide()
+		{
+			OnHide?.Invoke();
+		}
+	}
+}
